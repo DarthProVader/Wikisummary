97069653d2a4f031fd469c2f7c8c152c530ff7c9
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2021-09-11 18:06:52 +0200

    Initial commit

---
+++ wiki_hledacc.py
@@ -0,0 +1,64 @@
+import wikipedia
+
+wikipedia.set_lang("cz")
+
+db = {}
+
+def check_wiki_add_db(word):
+    try:
+        result = wikipedia.summary(word)
+        add_db = {word:result}
+        db.update(add_db)
+    except wikipedia.exceptions.DisambiguationError as err:
+        result = err.options
+    return result
+    
+def check_db(word):
+    result = False
+    for key in db.keys():
+        if word == key:
+            result = db[key] 
+            break
+        else:
+            result = False
+    return result
+
+def main():
+    
+    run = True
+
+    while run:
+
+        word_search = input("\nZadejte hledaný výraz: ") 
+        in_db = check_db(word_search)
+
+        if in_db != False:
+            print(in_db)
+        else:
+            try:
+                result = wikipedia.summary(word_search)
+                add_db = {word_search:result}
+                db.update(add_db)
+                print(result)
+            except wikipedia.exceptions.DisambiguationError as err:
+                print(f"Článek s tímto názvem: \"{word_search}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
+                for item in err.options:
+                    print(item)
+            except wikipedia.exceptions.PageError:
+                print("Zadaný výraz neexistuje, zkuste prosím jiný.")
+
+        again = True
+
+        while again:
+            quit_program = input("\nPřejete si hledat znovu? (a/n): ")
+            if quit_program.lower() == "n":
+                again = False
+                run = False
+                print("Ukončuji program")
+            elif quit_program.lower() != "a":
+                print("Nerozumím!")
+            else:
+                again = False
+
+if __name__ == '__main__':
+    main()
