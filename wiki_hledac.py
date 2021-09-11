340897f68302a2a4b58bc8c1d2a336a6e66f6b51
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2021-09-11 21:15:20 +0200

    Formatting, comments
    got rid of function which is not being used
    formatted code and added comments

--- wiki_hledac.py
+++ wiki_hledac.py
@@ -4,56 +4,59 @@ wikipedia.set_lang("cz")
 
 db = {}
 
-def check_wiki_add_db(word):
-    try:
-        result = wikipedia.summary(word)
-        add_db = {word:result}
-        db.update(add_db)
-    except wikipedia.exceptions.DisambiguationError as err:
-        result = err.options
-    return result
-    
+
 def check_db(word):
+    """this function checks database for previous searches"""
     result = False
     for key in db.keys():
         if word == key:
-            result = db[key] 
+            result = db[key]
             break
     return result
 
+
 def main():
-    
+
     run = True
 
     while run:
 
-        word_search = input("\nZadejte hledaný výraz: ") 
+        word_search = input("\nZadejte hledaný výraz: ")
+
+        # check for searched word in db and return content if there is any
         in_db = check_db(word_search)
 
         if in_db != False:
             print(in_db)
         else:
             try:
+                # get list of pages based on word search, if it matches return the page summary
                 search_list = wikipedia.search(word_search)
                 if search_list[0].lower() == word_search.lower():
                     result = wikipedia.summary(word_search)
-                    add_db = {word_search:result}
+                    add_db = {word_search: result}
                     db.update(add_db)
                     print(result)
+                # if not return list of pages containing search word
                 else:
-                    print(f"Článek s tímto názvem: \"{word_search}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
+                    print(
+                        f"Článek s tímto názvem: \"{word_search}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
                     for item in search_list:
                         print(item)
+            # return list of pages if searched word is a disambiguation page
             except wikipedia.exceptions.DisambiguationError as err:
-                print(f"Článek s tímto názvem: \"{word_search}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
+                print(
+                    f"Článek s tímto názvem: \"{word_search}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
                 for item in err.options:
                     print(item)
-            except wikipedia.exceptions.PageError:
+            # return page does not exists if nonsense input submitted
+            except wikipedia.exceptions.PageError and IndexError:
                 print("Zadaný výraz neexistuje, zkuste prosím jiný.")
 
         again = True
 
         while again:
+            # search again or quit
             quit_program = input("\nPřejete si hledat znovu? (a/n): ")
             if quit_program.lower() == "n":
                 again = False
@@ -63,6 +66,7 @@ def main():
                 print("Nerozumím!")
             else:
                 again = False
+
 
 if __name__ == '__main__':
     main()
