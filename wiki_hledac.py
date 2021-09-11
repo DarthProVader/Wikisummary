f6fd5c9a3851e9c6202f8cfa178ca514d4da137c
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2021-09-12 00:13:21 +0200

    Refactor code
    Created wiki_search function which is constructed from previous main code

--- wiki_hledac.py
+++ wiki_hledac.py
@@ -4,6 +4,31 @@ wikipedia.set_lang("cz")
 
 db = {}
 
+def wiki_search(word):
+    try:
+        # get list of pages based on word search, if it matches return the page summary
+        search_list = wikipedia.search(word)
+        if search_list[0].lower() == word.lower():
+            result = wikipedia.summary(word)
+            add_db = {word: result}
+            db.update(add_db)
+            print(result)
+        # if not return list of pages containing search word
+        else:
+            print(
+                f"Článek s tímto názvem: \"{word}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
+            for item in search_list:
+                print(item)
+    # return list of pages if searched word is a disambiguation page
+    except wikipedia.exceptions.DisambiguationError as err:
+        print(
+            f"Článek s tímto názvem: \"{word}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
+        for item in err.options:
+            print(item)
+    # return page does not exists if nonsense input submitted
+    except wikipedia.exceptions.PageError and IndexError:
+        print("Zadaný výraz neexistuje, zkuste prosím jiný.")
+
 
 def check_db(word):
     """this function checks database for previous searches"""
@@ -29,29 +54,7 @@ def main():
         if in_db != False:
             print(in_db)
         else:
-            try:
-                # get list of pages based on word search, if it matches return the page summary
-                search_list = wikipedia.search(word_search)
-                if search_list[0].lower() == word_search.lower():
-                    result = wikipedia.summary(word_search)
-                    add_db = {word_search: result}
-                    db.update(add_db)
-                    print(result)
-                # if not return list of pages containing search word
-                else:
-                    print(
-                        f"Článek s tímto názvem: \"{word_search}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
-                    for item in search_list:
-                        print(item)
-            # return list of pages if searched word is a disambiguation page
-            except wikipedia.exceptions.DisambiguationError as err:
-                print(
-                    f"Článek s tímto názvem: \"{word_search}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
-                for item in err.options:
-                    print(item)
-            # return page does not exists if nonsense input submitted
-            except wikipedia.exceptions.PageError and IndexError:
-                print("Zadaný výraz neexistuje, zkuste prosím jiný.")
+            wiki_search(word_search)
 
         again = True
 
