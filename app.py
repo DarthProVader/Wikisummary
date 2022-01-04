d1bcc0a604da30172ebfce5b9efded07eca329fb
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2022-01-04 16:17:48 +0100

    files restructure, search is wroking as expected

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ wikipedia.set_lang("cz")
 
 @app.route("/")
 def test_page():
-    return render_template("index.html")
+    return render_template("main_page.html")
 
 class WikiSearch(Resource):
+
     def get(self,word):
         """This function takes one string, searches wikipedia for that word and returns summary of the searched page. If page does not exist it returns list of suggestions"""
+
         try:
             # get list of pages based on word search, if first result matches the search return the page summary
             search_list = wikipedia.search(word)
@@ -34,6 +36,7 @@ class WikiSearch(Resource):
                     return {"suggested_pages":suggested_pages}
         # return list of pages if searched word is a disambiguation page
         except wikipedia.exceptions.DisambiguationError as err:
+
             print(
                 f"Článek s tímto názvem: \"{word}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
             suggested_pages = []
@@ -43,85 +46,11 @@ class WikiSearch(Resource):
                 return {"suggested_pages":suggested_pages}
         # return page does not exists if nonsense input submitted
         except wikipedia.exceptions.PageError and IndexError:
+
             print("Zadaný výraz neexistuje, zkuste prosím jiný.")
             return {'word':None},404
 
 api.add_resource(WikiSearch, "/search/<string:word>")
-
-
-
-#old code that will be used for api calls
-#cache will be handled in local storage
-#later DB will be added
-db = {}
-
-def wiki_search(word):
-    """This function takes one string, searches wikipedia for that word and returns summary of the searched page. If page does not exist it returns list of suggestions"""
-    try:
-        # get list of pages based on word search, if first result matches the search return the page summary
-        search_list = wikipedia.search(word)
-        if search_list[0].lower() == word.lower():
-            result = wikipedia.summary(word)
-            add_db = {word: result}
-            db.update(add_db)
-            print(result)
-        # if not return list of pages containing search word
-        else:
-            print(
-                f"Článek s tímto názvem: \"{word}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
-            for item in search_list:
-                print(item)
-    # return list of pages if searched word is a disambiguation page
-    except wikipedia.exceptions.DisambiguationError as err:
-        print(
-            f"Článek s tímto názvem: \"{word}\" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem:")
-        for item in err.options:
-            print(item)
-    # return page does not exists if nonsense input submitted
-    except wikipedia.exceptions.PageError and IndexError:
-        print("Zadaný výraz neexistuje, zkuste prosím jiný.")
-
-
-def check_db(word):
-    """This function takes one string and checks database for previous searches. If found it returns the value for searched key, if not it returns False"""
-    result = False
-    for key in db.keys():
-        if word == key:
-            result = db[key]
-            break
-    return result
-
-
-def main():
-
-    run = True
-
-    while run:
-
-        word_search = input("\nZadejte hledaný výraz: ")
-
-        # check for searched word in db and return content if there is any
-        in_db = check_db(word_search)
-
-        if in_db != False:
-            print(in_db)
-        else:
-            wiki_search(word_search)
-
-        again = True
-
-        while again:
-            # search again or quit
-            quit_program = input("\nPřejete si hledat znovu? (a/n): ")
-            if quit_program.lower() == "n":
-                again = False
-                run = False
-                print("Ukončuji program")
-            elif quit_program.lower() != "a":
-                print("Nerozumím!")
-            else:
-                again = False
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,74 @@
+let baseUrl = window.location.href
+
+function checkLocalStorage(word) {
+    let lsWord = localStorage.getItem(word)
+    if (lsWord != null) {
+        return lsWord
+    } else {
+        return null
+    }
+}
+
+document.querySelector(".search-form").addEventListener("submit", function (event) {
+    event.preventDefault()
+    document.querySelector(".second").innerHTML = ""
+
+    let searchedWord = event.target.elements.searchWiki.value
+
+    //check local storage for searched word if found write on the screen
+    if (checkLocalStorage(searchedWord) != null) {
+
+        let par = document.createElement("p")
+        par.innerHTML = checkLocalStorage(searchedWord)
+        document.querySelector(".second").appendChild(par)
+    }
+    //use API for finding the summary of the searched word
+    else {
+        let url = `${baseUrl}/search/${searchedWord}`
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
+            //server responds 404 - no such expression
+            .then(function (response) {
+                if (!response.ok) {
+
+                    let par = document.createElement("p")
+                    par.innerHTML = `Zadaný výraz neexistuje, zkuste prosím jiný.`
+                    document.querySelector(".second").appendChild(par)
+
+                    return null //for stopping the rest of the code from executing
+
+                }
+                return response.json()
+                    //200 - add to the local storage and write on screen 
+                    .then(data => {
+
+                        let sWord = Object.keys(data)[0]
+                        let res = Object.values(data)[0]
+
+                        if (typeof res == "string") {
+
+                            localStorage.setItem(sWord, res)
+
+                            console.log(res)
+
+                            let par = document.createElement("p")
+                            par.innerHTML = res
+                            document.querySelector(".second").appendChild(par)
+                        }
+                        //list of pages if searched word is a disambiguation page 
+                        else {
+                            console.log(`Článek s tímto názvem: "${searchedWord}" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem: ${data.suggested_pages}`)
+                            let par = document.createElement("p")
+                            par.innerHTML = `Článek s tímto názvem: "${searchedWord}" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem: ${data.suggested_pages}`
+                            document.querySelector(".second").appendChild(par)
+                        }
+
+                    })
+            })
+    }
+    event.target.elements.searchWiki.value = ""
+})
