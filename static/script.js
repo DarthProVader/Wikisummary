465c226b3346ee609f031e9b7247ea4256dcdcaa
Pavel Veselý <73229155+DarthProVader@users.noreply.github.com>
2022-01-10 21:12:22 +0100

    animation added, about page separated

--- static/script.js
+++ static/script.js
@@ -1,5 +1,20 @@
 let baseUrl = window.location.href
 
+let searchPage = document.querySelector(".search")
+let aboutPage = document.querySelector(".about")
+let resultPage = document.querySelector(".second")
+
+document.querySelector(".search-link").addEventListener("click", function(){
+    searchPage.style.display = "block"
+    resultPage.style.display = "block"
+    aboutPage.style.display = "none"
+})
+document.querySelector(".about-link").addEventListener("click", function(){
+    searchPage.style.display = "none"
+    resultPage.style.display = "none"
+    aboutPage.style.display = "block"
+})
+
 function checkLocalStorage(word) {
     let lsWord = localStorage.getItem(word)
     if (lsWord != null) {
@@ -9,11 +24,11 @@ function checkLocalStorage(word) {
     }
 }
 
+let loader = `<div class="loader">Loading...</div>`
+
 document.querySelector(".search-form").addEventListener("submit", function (event) {
     event.preventDefault()
-    /* let result = document.querySelector(".result")
-    console.log(result) */
-    document.querySelector(".second").innerHTML = ""
+    document.querySelector(".second").innerHTML = loader
 
     let searchedWord = event.target.elements.searchWiki.value
 
@@ -22,6 +37,7 @@ document.querySelector(".search-form").addEventListener("submit", function (even
 
         let par = document.createElement("p")
         par.innerHTML = checkLocalStorage(searchedWord)
+        document.querySelector(".second").innerHTML = ""
         document.querySelector(".second").appendChild(par)
     }
     //use API for finding the summary of the searched word
@@ -39,6 +55,7 @@ document.querySelector(".search-form").addEventListener("submit", function (even
 
                     let par = document.createElement("p")
                     par.innerHTML = `Zadaný výraz neexistuje, zkuste prosím jiný.`
+                    document.querySelector(".second").innerHTML = ""
                     document.querySelector(".second").appendChild(par)
 
                     return null //for stopping the rest of the code from executing
@@ -57,12 +74,14 @@ document.querySelector(".search-form").addEventListener("submit", function (even
 
                             let par = document.createElement("p")
                             par.innerHTML = res
+                            document.querySelector(".second").innerHTML = ""
                             document.querySelector(".second").appendChild(par)
                         }
                         //list of pages if searched word is a disambiguation page 
                         else {
                             let par = document.createElement("p")
                             par.innerHTML = `Článek s tímto názvem: "${searchedWord}" nebyl nalezen. Zadaný text se vysktuje v článcích s tímto názvem: <li> ${data.suggested_pages}`
+                            document.querySelector(".second").innerHTML = ""
                             document.querySelector(".second").appendChild(par)
                         }
 
